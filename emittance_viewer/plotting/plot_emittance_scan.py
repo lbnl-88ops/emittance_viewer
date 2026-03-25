05d9be3ff2d8bb17fbf1e20d667bfad295a86f2e
Jessica Rehak <rehak@lbl.gov>
2026-03-25 09:14:06 -0700

    updated comparison window, plotting

--- emittance_viewer/plotting/plot_emittance_scan.py
+++ emittance_viewer/plotting/plot_emittance_scan.py
@@ -45,7 +45,7 @@ def plot_file(ax, file: EmittanceScanFile):
     position = rms.x
     divergence = rms.xp
 
-    I_plot = rms.data * 1e9  # unit nA
+    I_plot = -rms.data * 1e9  # unit nA
     theta = np.linspace(0, 2 * np.pi, 100)
     E_rms = rms.e_rms * 1e6  # convert to mm mrad
     A = rms.alpha
@@ -59,7 +59,7 @@ def plot_file(ax, file: EmittanceScanFile):
     binlength_position = (max(position) - min(position)) / n
     binlength_divergence = (max(divergence) - min(divergence)) / m
     xx, xxp = np.meshgrid(position, divergence)
-    ax.pcolormesh(xx, xxp, I_plot.T, cmap="inferno")
+    cm = ax.pcolormesh(xx, xxp, I_plot.T, cmap="inferno")
     ax.plot(
         x_e,
         x_prime_e,
@@ -67,6 +67,7 @@ def plot_file(ax, file: EmittanceScanFile):
         label=r"$\epsilon_{rms}$ = " + f"{round(E_rms, 4)} [mm mrad]",
     )
     ax.plot(rms.x_mean * 1e3, rms.xp_mean * 1e3, "wx")
+    plt.colorbar(cm, ax=ax, label="Current (nA)")
     ax.set_xlim(rms.x[0], rms.x[-1])
     ax.set_ylim(rms.xp[0], rms.xp[-1])
     ax.set_title(file.formatted_datetime)
